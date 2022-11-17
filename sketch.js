d903189d363968c1074a40c2e37df5cf5f15444d
katharinechev <113962819+katharinechev@users.noreply.github.com>
2022-11-17 12:03:10 -0600

    initial commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+// Project Title
+// Katharine C
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
